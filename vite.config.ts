b7ae952bcab347f477a4eb56437d60891c9c8995
lettidude <lettidude@gmail.com>
2025-11-23 15:26:15 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -18,9 +18,9 @@ export default defineConfig({
     },
   },
   envDir: path.resolve(import.meta.dirname),
-  root: path.resolve(import.meta.dirname, "client"),
+  root: "client",
   build: {
-    outDir: path.resolve(import.meta.dirname, "../dist"),
+    outDir: path.resolve(import.meta.dirname, "dist"),
     emptyOutDir: true,
   },
   server: {
